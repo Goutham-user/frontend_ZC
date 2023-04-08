0a37169045d27b18332b73e165d7e8f7d649f2a5
goutham375 <goutham375@gmail.com>
2023-04-08 21:03:28 +0530

    version migration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import Router from './Components/Router'
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+const container = document.getElementById('root');
+const rooterContainer = ReactDOM.createRoot(container);
+rooterContainer.render(
   <React.StrictMode>
     <Router />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
